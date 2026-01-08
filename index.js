585bd8727291328489f821933898444291489da9
Rupali-132005 <rupaliyhin970@gmail.com>
2026-01-08 22:39:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const express = require("express");
+const express= require('express');
+const routes = require('./routes/users.js');
+const route = require('./routes/movies.js');
 
 const app = express();
+
 const PORT = 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.use(express.json()); // Middleware to parse JSON bodies
+
+app.use("/users",routes);
+app.use("/movies",route);
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
 });
